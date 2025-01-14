dc54ebabc22d692b88cb58099ef1075a388dcb04
AhmedBaid <150438476+AhmedBaid@users.noreply.github.com>
2025-01-14 16:27:50 +0100

    add result front end

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	css:=http.StripPrefix("/styles/",http.FileServer(http.Dir("styles")))
+	http.Handle("/styles/",css)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/hello", hello)
+	fmt.Println("Server running at: http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("index.html")
+	temp.Execute(w, nil)
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,"helo: hello")
+}
